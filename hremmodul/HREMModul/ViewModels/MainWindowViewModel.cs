6da8e241f13b2fa62322322917b7f3040a3db19f
HREMAdmin <79139634+HREMAdmin@users.noreply.github.com>
2021-02-16 14:20:41 +0100

    Add files via upload

---
+++ hremmodul/HREMModul/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading.Tasks;
+using System.Windows;
+using Prism.Commands;
+using Prism.Mvvm;
+
+namespace HREMModul.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        string messageBoxText = "Vil du gemme patienten i Høremappen?";
+        string caption = "Gem patient i Høremappe ";
+        MessageBoxButton button = MessageBoxButton.OKCancel;
+        MessageBoxImage icon = MessageBoxImage.Warning;
+        private string _title = "Høremappe modul";
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+        public MainWindowViewModel()
+        {
+            
+            // Display message box
+            
+        }
+
+        public DelegateCommand dialog => new DelegateCommand(async () => await ShowDialog());
+
+        private async Task ShowDialog()
+        {
+            MessageBox.Show(messageBoxText, caption, button, icon);
+        }
+    }
+}
